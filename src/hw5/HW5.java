8d4014769d2f438f21eb679afcf7ffd4cd045e9b
tommydo1991 <tommy_d91@yahoo.com>
2014-03-16 23:20:45 -0700

    new commit

---
+++ src/hw5/HW5.java
@@ -0,0 +1,50 @@
+package hw5;
+import java.util.Scanner;
+public class HW5 {
+
+
+    public static void main(String[] args) {
+       int thirsty;
+        int breakfast;
+ 
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("hungry");
+        
+        System.out.print("get in line");
+        
+        System.out.print("buy lunch");
+        
+        System.out.print("thirsty");
+        
+        System.out.print("enter 1 if thirsty");
+        thirsty = input.nextInt();
+        if(thirsty == 1)
+        {
+            System.out.print("buy a dietcoke");
+        }
+        else
+        {
+            System.out.print("buy a water");
+        }
+        
+        System.out.print("enter 2 if had breakfast");
+        breakfast = input.nextInt();
+        if(breakfast == 2)
+        {
+            System.out.print("buy a coke");
+        }
+  else
+        {
+            System.out.print("buy a water");
+        }
+        
+        System.out.print("eat lunch");
+        
+        System.out.print("return tray");
+        
+        System.out.print("leave");
+        
+    }
+    
+}
